4f371525598ab84b919f9603472de49de61ebe96
Brayan Carbono <yambra12@gmail.com>
2020-11-14 16:19:44 -0500

    creacion de modelos

--- app/src/main/java/com/example/personas/modelos/Persona.java
+++ app/src/main/java/com/example/personas/modelos/Persona.java
@@ -7,11 +7,34 @@ public class Persona {
     private String apellido;
 
     Persona(String cedula, String nombre, String apellido){
-        this.cedula = cedula;
-        this.nombre = nombre;
-        this.apellido = apellido;
+        this.setCedula(cedula);
+        this.setNombre(nombre);
+        this.setApellido(apellido);
     }
 
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
 
     void guardar(){
         Datos.guardar(this);
